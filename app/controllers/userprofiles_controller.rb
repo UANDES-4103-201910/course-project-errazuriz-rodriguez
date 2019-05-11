2388c330a53ed9f580422356f8fc297d76368f6d
juanesgh <jerodriguez@miuandes.cl>
2019-05-10 22:16:12 -0400

    Edititng views

--- app/controllers/userprofiles_controller.rb
+++ app/controllers/userprofiles_controller.rb
@@ -1,5 +1,7 @@
 class UserprofilesController < ApplicationController
 
     def userprofile
+        a1 = 1 #current user con las sesion creada se cambia esre valor
+        @usr = User.where(id: a1).first
     end
 end
