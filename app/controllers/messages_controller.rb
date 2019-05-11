5b1f0212ceb8a591704bf56f4b30737043e41581
juanesgh <jerodriguez@miuandes.cl>
2019-05-10 20:19:25 -0400

    Links

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,5 @@
 class MessagesController < ApplicationController
-    def index
-        #Agregar links con las conversaciones a otros usuarios,
-        #al ghacer click al link abrir conversacion con un nuevo def
-    end
+
     def create
         @message = Message.new(message_params)
     end
@@ -10,24 +7,30 @@ class MessagesController < ApplicationController
         #muestra mensajes anteriores y da la posibilidad  de escribir uno nuevo
         #en caso de enviar uno nuevo, llamar a create
         
-        @u1 = 1#por mientras
-        u2 = @selected_chat
-        @message = Messages.all.where(sender_id = u1, receiver_id = u2 or sender_id = u2, receiver_id = u1).order(:created_at)
+        @u1 = 1#por mientras, se ingresa el id del usuario actual
+        u2 = 2#va con @selected chat de chats, chat lleva a conversation, con un click listener
+        @message = Message.where(:sender_id => @u1, :receiver_id => u2).or(Message.where(:sender_id => u2, :receiver_id => @u1)).order(:created_at)# OR sender_id = u2, receiver_id = u1).order(:created_at)
         
     end
 
     def chats
         #mustra los chats que se han realizado con otros usuarios
+        @usr = User.all
         @selected_chat = 2
-        chats = Messages.all.where(sender_id = @u1 or receiver_id = @u1)
+        chats = Message.where(sender_id = @u1).or(Message.where(receiver_id = @u1))
         @u3 = []
+        @u3_nick = []
         chats.each do |ch|
             if ((ch.sender_id != @u1) and (!(ch.sender_id.in?(chats))))
                 @u3.push(ch.sender_id)
+                @u3_nick.push(User.find_by(:id => ch.sender_id).nick)
             elsif (ch.receiver_id != @u1) and !(ch.receiver_id.in?(chats))
                 @u3.push(ch.receiver_id)
+                @u3_nick.push(User.find_by(:id => ch.receiver_id).nick)
             end
         #en el view hacer bucle sobre chats, clickeables mostrando el nombre
+        @lin = @u3.length
+        end
     end
 
     def new
--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,15 @@ Rails.application.routes.draw do
   get '/changepassword', to: 'changepasswords#changepassword'
   get '/dumpster', to: 'dumpsters#dumpster'
   get '/editprofile', to: 'editprofiles#editprofile'
-  get '/home', to: 'homes#home'
-  get '/passrec', to: 'passrecs#passrec'
+  get '/home', to: 'homes#home'##########
+  get '/passrec', to: 'passrecs#passrec'########
   get '/reg', to: 'regs#reg'
   get '/useradmin', to: 'useradmins#useradmin'
   get '/userhome', to: 'userhomes#userhome'
   get '/userprofile', to: 'userprofiles#userprofile'
   get '/adminsystem', to: 'adminsystems#adminsystem'
+  get '/messages/conversation', to: 'messages#conversation'
+  get '/messages', to: 'messages#chats'
 
 
 
